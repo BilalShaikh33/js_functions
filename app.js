fa651c60378677cc1ba110c3f97a9ea12dcd29ef
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-10-24 06:18:13 -0700

    i

--- app.js
+++ app.js
@@ -9,36 +9,53 @@
 
 
 
-const calculator = (a, b, symbol) => {
-    let result;
-    if (symbol == "+") {
-        result = a + b
+// const calculator = (a, b, symbol) => {
+//     let result;
+//     if (symbol == "+") {
+//         result = a + b
 
+//     }
+//     if (symbol == "-") {
+//         result = a - b
+
+//     }
+//     if (symbol == "*") {
+//         result = a * b
+
+//     }
+//     if (symbol == "/") {
+//         result = a / b
+
+//     }
+//     else {
+//         result = "Invalid operator"
+//     }
+//     return result
+
+
+
+// }
+
+// var n1 = +prompt("Enter first number: ")
+// var n2 = +prompt("Enter second number: ")
+// var op = prompt("Enter operator(+,-,*,/): ")
+// console.log(calculator(n1, n2, op))
+
+
+
+const vowelChecker = (a) => {
+
+    let checker;
+    if (a == "a" || a == "e" || a == "i" || a == "o" || a == "u") {
+        checker = "It is a vowel"
     }
-    if (symbol == "-") {
-        result = a - b
-
+    else {
+        checker = "It is not a vowel"
     }
-    if (symbol == "*") {
-        result = a * b
-
-    }
-    if (symbol == "/") {
-        result = a / b
-
-    }
-    return result
-
-    
-    
+    return checker
 }
 
-var n1 = +prompt("Enter first number: ")
-var n2 = +prompt("Enter second number: ")
-var op = prompt("Enter operator(+,-,*,/): ")
-console.log(calculator(n1, n2, op))
 
-
-
-
+var letter = prompt("Enter any character: ").toLowerCase()
+console.log(vowelChecker(letter))
 

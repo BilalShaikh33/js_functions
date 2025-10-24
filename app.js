a517b4d752d3bdd34264145b5ad705ea2331510f
Bilal Shaikh <bilalshaikhh339@gmail.com>
2025-10-24 06:03:16 -0700

    i

--- app.js
+++ app.js
@@ -1,11 +1,39 @@
-function sum(a,b) {
-    // console.log("Sum of a and b is : ", a+b)
+// function sum(a,b) {
+//     // console.log("Sum of a and b is : ", a+b)
 
-    return (a+b)
+//     return a+b
+// }
+
+// let num = sum(2,3);
+// console.log(num);
+
+
+
+const calculator = (a,b,symbol) =>{
+    let result;
+    if (symbol == "+") {
+        result = a+b
+        
+    }
+    if (symbol == "-") {
+        result = a-b
+        
+    }
+    if (symbol == "*") {
+        result = a*b
+        
+    }
+    if (symbol == "/") {
+        result = a/b
+        
+    }
+    return result
+  
+
+
+
 }
 
-let num = sum(2,3);
-console.log(num);
 
 
 
